5ff4be0c92f2bbc646b617b0186d31df1c80fd52
Yuichi Takeuchi <yuichi-takeuchi@umin.net>
2017-05-27 15:47:09 +0200

    Uploading files

---
+++ MTOPrep.m
@@ -0,0 +1,13 @@
+% 
+% Yuichi Takeuchi, Ph.D., Department of Physiology, University of Szeged
+% yuichi-takeuchi@umin.net
+% Yuichi Takeuchi (c) 2017
+%% load
+load('MTO.mat')
+
+%%
+MTO
+
+%%
+guide MTO.fig
+
